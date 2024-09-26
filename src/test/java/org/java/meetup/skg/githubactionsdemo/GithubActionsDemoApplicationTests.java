b41c0de7ff0e265b904202c770cf47fa22ad275e
Eleftherios Chrysochoidis <lefterisxris@gmail.com>
2024-09-26 20:00:12 +0300

    trigger failure

--- src/test/java/org/java/meetup/skg/githubactionsdemo/GithubActionsDemoApplicationTests.java
+++ src/test/java/org/java/meetup/skg/githubactionsdemo/GithubActionsDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package org.java.meetup.skg.githubactionsdemo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class GithubActionsDemoApplicationTests {
 
    @Test
    void contextLoads() {
+      Assertions.assertEquals(5,3);
    }
 
 }
